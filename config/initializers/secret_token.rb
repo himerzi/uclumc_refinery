4cf6ca86dffb53e810f0e13e20566ba1af1adacc
Michael Detmold <michael.detmold@thatcrowdthing.com>
2011-08-01 20:49:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uclumc::Application.config.secret_token = 'ee7ff9b103f46c621254f4da8314a113baf7357217063809ea87bf733f4db607247b11b23435660b77cb307de0af45dd05a18a6dd7a0a2704b259710ed357918'
